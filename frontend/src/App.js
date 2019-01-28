1b35540f141a43adf448bc4a1b46aa3b4f8ef0d7
Marcos Harbs <marcos.harbs@gmail.com>
2019-01-28 11:07:27 -0200

    classe para cosumir a api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,80 @@
 import React, { Component } from 'react';
+import { 
+  getCategories, 
+  getCategoryPosts,
+  getPosts,
+  addPost,
+  upVotePost,
+  downVotePost,
+  editPost,
+  deletePost,
+  getComments,
+  addComment,
+  upVoteComment,
+  downVoteComment,
+  editComment,
+  deleteComment 
+} from './api/ReadableApi'
 
 class App extends Component {
+
+  componentDidMount() {
+    getCategories()
+    .then(categories => console.log('getCategories', categories))
+
+    getCategoryPosts('react')
+    .then(posts => console.log('getCategoryPosts', posts))
+
+    getPosts()
+    .then(posts => console.log('getPosts', posts))
+
+    addPost({
+      id: 'unique-2',
+      timestamp: Date.now(),
+      title: 'teste',
+      body: 'teste teste',
+      author: 'harbs',
+      category: 'react'
+    })
+    .then(post => console.log('addPost', post))
+
+    upVotePost('unique-2')
+    .then(post => console.log('upVotePost', post))
+
+    downVotePost('unique-2')
+    .then(post => console.log('downVotePost', post))
+
+    editPost('unique-2', 'title edited', 'body edited')
+    .then(post => console.log('editPost', post))
+
+    deletePost('unique-2')
+    .then(post => console.log('deletePost', post))
+
+    getComments('8xf0y6ziyjabvozdd253nd')
+    .then(comments => console.log('getComments', comments))
+
+    addComment({
+      id: 'comment-1',
+      timestamp: Date.now(),
+      body: 'teste comment',
+      author: 'harbs',
+      parentId: '8xf0y6ziyjabvozdd253nd'
+    })
+    .then(comment => console.log('addComment', comment))
+
+    upVoteComment('comment-1')
+    .then(comment => console.log('upVoteComment', comment))
+
+    downVoteComment('comment-1')
+    .then(comment => console.log('downVoteComment', comment))
+
+    editComment('comment-1', Date.now(), 'comment edited')
+    .then(comment => console.log('editComment', comment))
+
+    deleteComment('comment-1')
+    .then(comment => console.log('deleteComment', comment))
+  }
+
   render() {
     return (
       <div>Hello World!</div>
---
+++ frontend/src/api/ReadableApi.js
@@ -0,0 +1,90 @@
+import axios from 'axios'
+
+const client = axios.create({
+    baseURL: 'http://localhost:3001',
+    headers: { 'Authorization': '123u2i1y' }
+})
+
+export const getCategories = async () => {
+    const response = await client.get('/categories')
+    const data = await response.data
+    return data.categories
+}
+
+export const getPosts = async () => {
+    const response = await client.get('/posts')
+    const data = await response.data
+    return data
+}
+
+export const getCategoryPosts = async (category) => {
+    const response = await client.get(`/${category}/posts`)
+    const data = await response.data
+    return data
+} 
+
+export const addPost = async (post) => {
+    const response = await client.post('/posts', post)
+    const data = await response.data
+    return data
+}
+
+export const editPost = async (id, title, body) => {
+    const response = await client.put(`/posts/${id}`, { title, body })
+    const data = await response.data
+    return data
+}
+
+export const deletePost = async (id) => {
+    const response = await client.delete(`/posts/${id}`)
+    const data = await response.data
+    return data
+}
+
+export const upVotePost = async (id) => {
+    const response = await client.post(`/posts/${id}`, { option: 'upVote' })
+    const data = await response.data
+    return data
+}
+
+export const downVotePost = async (id) => {
+    const response = await client.post(`/posts/${id}`, { option: 'downVote' })
+    const data = await response.data
+    return data
+}
+
+export const getComments = async (id) => {
+    const response = await client.get(`/posts/${id}/comments`)
+    const data = await response.data
+    return data
+}
+
+export const addComment = async (comment) => {
+    const response = await client.post('/comments', comment)
+    const data = await response.data
+    return data
+}
+
+export const upVoteComment = async (id) => {
+    const response = await client.post(`/comments/${id}`, { option: 'upVote' })
+    const data = await response.data
+    return data
+}
+
+export const downVoteComment = async (id) => {
+    const response = await client.post(`/comments/${id}`, { option: 'downVote' })
+    const data = await response.data
+    return data
+}
+
+export const editComment = async (id, timestamp, body) => {
+    const response = await client.put(`/comments/${id}`, { timestamp, body })
+    const data = await response.data
+    return data
+}
+
+export const deleteComment = async (id) => {
+    const response = await client.delete(`/comments/${id}`)
+    const data = await response.data
+    return data
+}
