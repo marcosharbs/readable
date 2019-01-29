c45f223457c92e4310120853f193145dba12393c
Marcos Harbs <marcos.harbs@gmail.com>
2019-01-28 22:16:59 -0200

    filtro por categoria e ordenação

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -4,7 +4,21 @@ import { Grid, Select, MenuItem } from '@material-ui/core';
 
 class PostList extends Component {
 
+    state = {
+        field: 'voteScore',
+        order: 'desc'
+    }
+
+    changeField = (field) => {
+        this.setState({ field })
+    }
+
+    changeOrder = (order) => {
+        this.setState({ order })
+    }
+
     render() {
+        const { field, order } = this.state
         const { posts, match } = this.props
         
         let filteredPosts = posts
@@ -14,12 +28,15 @@ class PostList extends Component {
             filteredPosts = filteredPosts.filter(post => post.category === category)
         }
 
+        filteredPosts = filteredPosts.sort((a, b) => order === 'asc' ? a[field] - b[field] : b[field] - a[field])
+
         return (
             <Grid container >
                 <label style={{ marginLeft: '15px', marginTop: '45px' }}>Order by: </label>
                 <Grid item xs={5} sm={2}>
-                    <Select 
-                        value={'voteScore'}
+                    <Select
+                        onChange={event => this.changeField(event.target.value)} 
+                        value={field}
                         style={{ 
                             width: '100%', 
                             marginLeft: '15px', 
@@ -31,19 +48,20 @@ class PostList extends Component {
                 </Grid>
                 <Grid item xs={5} sm={2}>
                     <Select 
-                        value={'asc'}
+                        onChange={event => this.changeOrder(event.target.value)}
+                        value={order}
                         style={{ 
                             width: '100%', 
                             marginLeft: '40px', 
                             marginTop: '40px' 
                         }}>
-                        <MenuItem value={'asc'}>Ascending</MenuItem>
-                        <MenuItem value={'desc'}>Descending</MenuItem>
+                        <MenuItem value={'asc'}>A-Z</MenuItem>
+                        <MenuItem value={'desc'}>Z-A</MenuItem>
                     </Select>
                 </Grid>
                 <Grid item xs={12}>
                     {filteredPosts.length > 0 ? filteredPosts.map(post => (
-                        <p key={post.id}>{`${post.id} - ${post.title}`}</p>
+                        <p key={post.id}>{`${post.voteScore} - ${post.title}`}</p>
                     )) : (<p>{'Posts not found!'}</p>)}
                 </Grid>
             </Grid>
